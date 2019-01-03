51826a59f13bc625da5c8b226bc552cbe69da1fa
Jaspalsinh Chauhan <jachauhan@gmail.com>
2019-01-03 16:54:52 +0530

    6. Decrement value of greenbottles and print again

--- main.cpp
+++ main.cpp
@@ -6,5 +6,8 @@ int main()
 {
     int greenBottles = 10;
     cout << "There were " << greenBottles << " green bottles" << endl;
+
+    greenBottles--;
+    cout << "There were " << greenBottles << " green bottles" << endl;
     return 0;
 }
